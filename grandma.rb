dbda726d30d35045ce29b7f4f445ca682632c167
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-02 14:02:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase != "I LOVE YOU GRANDMA!" && phrase != phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!""
-  elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
-    puts"NO, NOT SINCE 1938!"
+    puts "UH?! SPEAK UP, SONNY!"
+  elsif phrase = phrase && phrase != "I LOVE YOU GRANDMA!"
+    puts "NO, NOT SINCE 1938!"
   else
-    "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
 end
